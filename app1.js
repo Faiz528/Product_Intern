8ec98aab8aa74e0c7f38e1849dd09d67ec1af627
MD FAIZUDDIN <84907848+Faiz528@users.noreply.github.com>
2024-02-12 02:51:39 +0530

    packages uploaded

---
+++ app1.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const express = require('express')
+const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+const Parser = require('body-parser')
+
+const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./model/user')
+const Expenses = require('./model/expense')
+
+const helmet = require('helmet')
+const compressions = require('compression')
+const morgan = require('morgan')
+
+const accessLog = fs.createWriteStream(
+  path.join(__dirname,'access.log'),{flags:'a'}
+)
+app.use(Parser.json({extended:false}))
+app.use(cors())
+app.use(cors({
+  origin: '*'
+}));
+app.use(express.static('public'));
+app.use(helmet())
+app.use(compressions())
+app.use(morgan('combined',{stream:accessLog}))
+
+
+
+const Signup = require('./expenses/route/sign')
+app.use(Signup)
+const Login = require('./expenses/route/log')
+app.use(Login)
+const Expense = require('./expenses/route/expense')
+app.use(Expense)
+
+
+
+
+
+
+mongoose.connect('mongodb+srv://Faiz:Sharpener2023@faizuddin.tyr9tuj.mongodb.net/Product?retryWrites=true&w=majority').then(result=>{
+    console.log("Connected")
+    app.listen(3000)
+}).catch(err=>{
+    console.log(err)
+})
+
