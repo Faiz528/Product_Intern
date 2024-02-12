82f4fcd86c6d11eade7924dd075c874c158f4427
MD FAIZUDDIN <84907848+Faiz528@users.noreply.github.com>
2024-02-12 12:33:44 +0530

    Update app1.js

--- app1.js
+++ app1.js
@@ -40,11 +40,12 @@ app.use(Expense)
 
 
 
-
+const PORT = process.env.PORT || 4000;
 
 mongoose.connect('mongodb+srv://Faiz:Sharpener2023@faizuddin.tyr9tuj.mongodb.net/Product?retryWrites=true&w=majority').then(result=>{
     console.log("Connected")
-    app.listen(3000)
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 }).catch(err=>{
     console.log(err)
 })
